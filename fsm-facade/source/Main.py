87d6b7feeefcc8a09db2bdd1c4b94be36552b579
rafalk42 <rafalk42@gmail.com>
2017-03-09 22:34:39 +0100

    Add threading and queues in ProcessExecutor

--- fsm-facade/source/Main.py
+++ fsm-facade/source/Main.py
@@ -3,8 +3,17 @@
 import sys
 import getopt
 import time
+import signal
 import Configuration
 import ProcessExecutor
+
+executor = None
+def signal_handler(signal, frame):
+	print "You pressed Ctrl+C!"
+	stdinQueue = executor.getStdinQueue()
+	stdinQueue.put ("/quit")
+	executor.wait()
+	print "Process quit"
 
 def usage (myself):
 	print "Usage " + myself + " [OPTIONS]"
@@ -12,6 +21,9 @@ def usage (myself):
 	print "  -c, --config <path>\t\tpath to configuration file"
 
 def main (argv):
+	global executor
+	signal.signal(signal.SIGINT, signal_handler)
+	
 	try:
 		opts, args = getopt.getopt (argv[1:], "c:h", ["config=", "help"])
 	except getopt.GetoptError as ex:
@@ -41,21 +53,31 @@ def main (argv):
 	
 	executor = ProcessExecutor.ProcessExecutor (config.getBinPath())
 	executor.start()
+	stdoutQueue = executor.getStdoutQueue()
+	stdinQueue = executor.getStdinQueue()
 	
 	time.sleep(0.5)
 	
-	lines = executor.readLines()
-	for line in lines:
+	#lines = executor.readLines()
+	#for line in lines:
+	#	print "OUT: " + line
+	#executor.writeLine ("/time")
+	#time.sleep (0.1)
+	#executor.writeLine ("/quit")
+	#while executor.isRunning():
+	#	lines = executor.readLines()
+	#	for line in lines:
+	#		if len (line) > 0:
+	#			print "OUT: " + line
+	#executor.wait()
+	
+	stdinQueue.put ("/time")
+	while executor.isRunning() or not stdoutQueue.empty():
+		line = stdoutQueue.get (True)
+		#print repr (line)
+		#if len (line) > 0:
 		print "OUT: " + line
-	executor.writeLine ("/time")
-	time.sleep (0.1)
-	executor.writeLine ("/quit")
-	while executor.isRunning():
-		lines = executor.readLines()
-		for line in lines:
-			if len (line) > 0:
-				print "OUT: " + line
-	executor.wait()
+
 
 if __name__ == "__main__":
 	main (sys.argv)
--- fsm-facade/source/ProcessExecutor.py
+++ fsm-facade/source/ProcessExecutor.py
@@ -1,6 +1,9 @@
 import time
 import subprocess
 import select
+import threading
+import Queue
+
 
 class ProcessExecutor:
 	def __init__ (self, binPath):
@@ -13,40 +16,99 @@ class ProcessExecutor:
 [self.binPath, "--server-settings", "../../../factorio/dedicated/server-settings.json", "--start-server", "../../../factorio/dedicated/save.zip"],
 stdin=subprocess.PIPE,
 stdout=subprocess.PIPE)
+			
+			self.stdoutLinesQueue = Queue.Queue()
+			self.stdinLinesQueue = Queue.Queue()
+			self.readerThread = ProcessStdoutReader (proc.stdout, self.stdoutLinesQueue)
+			self.readerThread.start()
+			
+			self.writerThread = ProcessStdinWriter (proc.stdin, self.stdinLinesQueue)
+			self.writerThread.start()
+			
 			self.proc = proc
 			self.stdout = proc.stdout
 			self.stdin = proc.stdin
-			#while proc.poll() is None:
-			#	proc.stdin.write ("/quit\n")
 		except OSError as ex:
 			print "Error executing binary"
 			print repr (ex)
 		print "Process has been started"
 	
-	def writeLine (self, text):
-		self.stdin.write (text + "\n")
+	def getStdoutQueue (self):
+		return self.stdoutLinesQueue
 	
-	def readLines (self):
-		output = []
-		
-		while True:
-			inputdata, outputdata, exceptions = select.select ([self.stdout], [], [self.stdout], 0)
-			if self.stdout in inputdata:
-				line = self.stdout.readline()
-				output.append (line.rstrip())
-			if self.stdout in exceptions:
-				print "EXCEPTION"
-				break;
-			else:
-				break
-		
-		return output
+	def getStdinQueue (self):
+		return self.stdinLinesQueue
 	
 	def wait (self):
 		self.proc.wait()
+		
+		print "Notifying threads to stop"
+		self.readerThread.stop()
+		self.writerThread.stop()
+		
+		print "Waiting for threads to end"
+		self.readerThread.join()
+		self.writerThread.join()
+		print "All threads clear"
 	
 	def isRunning (self):
 		if self.proc.poll() is None:
 			return True
 		else:
 			return False
+
+class ProcessStdoutReader (threading.Thread):
+	def __init__ (self, stdout, queue):
+		threading.Thread.__init__ (self)
+		self.stdout = stdout
+		self.queue = queue
+		self.exit = False
+	
+	def run (self):
+		print "ProcessStdoutReader started"
+		
+		while not self.exit:
+			try:
+				inputdata, outputdata, exceptions = select.select ([self.stdout], [], [self.stdout], 0.1)
+			except select.error, v:
+				if v[0] != errno.EINTR:
+					print "EINTR"
+					raise
+				else:
+					print repr (v)
+					break
+			if self.stdout in inputdata:
+				line = self.stdout.readline()
+				lineStripped = line.rstrip()
+				if len (lineStripped) > 0:
+					self.queue.put (lineStripped)
+			if self.stdout in exceptions:
+				print "EXCEPTION"
+				break;
+		
+		print "ProcessStdoutReader finished"
+	
+	def stop (self):
+		self.exit = True
+
+class ProcessStdinWriter (threading.Thread):
+	def __init__ (self, stdin, queue):
+		threading.Thread.__init__ (self)
+		self.stdin = stdin
+		self.queue = queue
+		self.exit = False
+	
+	def run (self):
+		print "ProcessStdinWriter started"
+		
+		while not self.exit:
+			try:
+				line = self.queue.get (True, 0.1)
+				self.stdin.write (line + "\n")
+			except Queue.Empty:
+				pass
+		
+		print "ProcessStdoutWriter finished"
+	
+	def stop (self):
+		self.exit = True
